a133c16916c033a51dc032f29521d6e854c0aa66
Marina Dvoretskova <smallestproblem@gmail.com>
2024-11-08 15:22:01 +0300

    Initial commit

---
+++ src/test/java/ru/yanmayak/romaskaco/RomaskaCoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.yanmayak.romaskaco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomaskaCoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
